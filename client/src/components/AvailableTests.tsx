a48acbc84db62a92d415e1309ec32c1e884a3c03
Laurentiu Popa <popa.laur.01@gmail.com>
2024-02-12 22:01:52 +0200

    Features card implemented

--- client/src/components/AvailableTests.tsx
+++ client/src/components/AvailableTests.tsx
@@ -1,5 +1,12 @@
 
 export default function AvailableTests() {
+    fetch('https://graph.brokee.io/v1/graphql?q=GetTests', {
+	method: 'POST',
+	headers: {'Content-Type': 'application/json'},
+    })
+	.then(res => res.json())
+	.then(res => console.log(res.data));
+    
   return (
     <div>AvailableTests</div>
   )
--- client/src/components/Features.tsx
+++ client/src/components/Features.tsx
@@ -1,6 +1,17 @@
+import Pricing from '../assets/pricing.png'
 
 export default function Features() {
+
   return (
-    <div>Features</div>
+    <div className="div-features">
+        <div className='off-div'>
+            <img src={Pricing} />
+            <div className='text-features'>
+                <h3>Introducing Pricing Plans and Free Trial for DevOps Tests</h3>
+                <p>We're starting 2024 off with a bang! We are happy to announce our biggest product update yet: the release of payment plans with a free trial.</p>
+                <a href="https://blog.brokee.io/free-trial-devops-test-pricing/" target="_blank" rel="noopener noreferrer" >Read more</a>
+            </div>
+        </div>
+    </div>
   )
 }
